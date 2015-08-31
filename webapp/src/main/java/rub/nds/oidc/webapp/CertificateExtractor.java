4d5b3370d07c6b64e354984f083e452f18152be2
philipp <philipp@big-foot2>
2015-08-31 19:14:24 +0200

    Implemented user certificate extraction The authenticationa and token servlets extract the user certificate and throw an exception if non is found.

---
+++ webapp/src/main/java/rub/nds/oidc/webapp/CertificateExtractor.java
@@ -0,0 +1,36 @@
+package rub.nds.oidc.webapp;
+
+import java.security.cert.X509Certificate;
+import javax.servlet.http.HttpServletRequest;
+import rub.nds.oidc.exceptions.OIDCUserCertificateNotFoundException;
+
+/**
+ * Certificate extractor.
+ * 
+ * @author Philipp Markert <philipp.markert@rub.de>
+ */
+public class CertificateExtractor {
+
+    static X509Certificate[] certificateChain;
+
+    protected CertificateExtractor() {
+    }
+
+    /**
+     * Extracts the certificate from a HTTPServletRequest.
+     * 
+     * @param request The HTTPServletRequest of which the certificate should be extracted
+     * 
+     * @return The X.509 certificate which is used in the request
+     * 
+     * @throws OIDCUserCertificateNotFoundException If no X.509 certificat can be found in the request
+     */
+    protected static X509Certificate extractCertificate(HttpServletRequest request) 
+            throws OIDCUserCertificateNotFoundException {
+        certificateChain = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
+        if (null != certificateChain && certificateChain.length > 0) {
+            return certificateChain[0];
+        }
+        throw new OIDCUserCertificateNotFoundException("No X.509 client certificate found in request");
+    }
+}
