fe175cbb010360f94bbea5c7fb4abe3d41a52a58
philipp <philipp@big-foot2>
2015-09-01 21:08:21 +0200

    Configuration Manager previous version without schema validation

--- oidc_op/src/test/java/rub/nds/oidc/oidc_op/ConfigurationManagerTest.java
+++ oidc_op/src/test/java/rub/nds/oidc/oidc_op/ConfigurationManagerTest.java
@@ -1,90 +1,39 @@
 package rub.nds.oidc.oidc_op;
 
-import com.thoughtworks.xstream.converters.reflection.AbstractReflectionConverter.UnknownFieldException;
 import java.util.List;
-import static junit.framework.TestCase.assertTrue;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import junit.framework.TestCase;
 
 /**
  *
  * @author Vladislav Mladenov<vladislav.mladenov@rub.de>
  */
-public class ConfigurationManagerTest {
-    @Rule public ExpectedException thrown= ExpectedException.none();
+public class ConfigurationManagerTest extends TestCase {
 
-    public ConfigurationManagerTest() {
+    public ConfigurationManagerTest(String testName) {
+        super(testName);
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
     }
 
     /**
      * Test of init method, of class ConfigurationManager.
      */
-    @Test
     public void testInit() {
         ConfigurationManager.initialize();
 
         List<Client> clientDB = OIDCCache.getCfgDB().getClientDatabase();
-        assertTrue(clientDB.get(0).getClient_id().equalsIgnoreCase("Ek1P6CVtW9fNIRfZEyMyCanEoFUfjcNLWuxcPVmCJrU"));
-        assertTrue(clientDB.get(0).getClient_secret().equalsIgnoreCase("P1vhVxcD2BNY0kPzyrQAOcnLkrOH8A0wkRysGocU0U8"));
-        assertTrue(clientDB.get(1).getName().equalsIgnoreCase("Cloud NDS Test App 2"));
+
+        for (Client c : clientDB) {
+            System.out.println("Client: " + c.getName());
+        }
     }
 
-    /**
-     * Test of init method, of class ConfigurationManager.
-     */
-    @Test
-    public void testInitWithErrors() {
-        thrown.expect( RuntimeException.class );
-        thrown.expectMessage("Cannot initialize configurationManager!");
-        ConfigurationManager.initialize("nonExistingFile.xml", "configDatabaseSchema.xsd");
-    }
-    
-    /**
-     * Test of init method, of class ConfigurationManager.
-     */
-    @Test
-    public void testInitWithErrors2() {
-        
-        thrown.expect(RuntimeException.class);
-        ConfigurationManager.initialize("configDatabase_Errors.xml", "configDatabaseSchema.xsd");
-    }
-    
-    /**
-     * Test of init method, of class ConfigurationManager.
-     */
-    @Test
-    public void testInitWithErrors3() {
-        thrown.expect( RuntimeException.class );
-        ConfigurationManager.initialize("configDatabase_Errors_1.xml", "configDatabaseSchema.xsd");
-    }
-    
-    /**
-     * Test of init method, of class ConfigurationManager.
-     */
-    @Test
-    public void testInitWithErrors4() {
-        thrown.expect( RuntimeException.class );
-        ConfigurationManager.initialize("configDatabase_Errors_2.xml", "configDatabaseSchema.xsd");
-    }
 }
